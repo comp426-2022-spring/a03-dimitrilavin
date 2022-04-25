72b578f38f47227f53bd840b4368d5285bb8cff0
dimitrilavin <dlavin@live.unc.edu>
2022-04-25 16:42:05 -0400

    server

--- server.js
+++ server.js
@@ -4,4 +4,6 @@ const args = require('minimist')(process.argv.slice(2))
 args['port']
 const port = args.port || process.env.PORT || 5000
 
-
+const server = app.listen(HTTP_PORT, () => {
+    console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
+});
